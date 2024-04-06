a4605150d98d5eb041a5cef7323f2f83288f7324
itx.alihamza <itx.alihamza.pro@gmail.com>
2024-04-06 10:41:40 +0500

    comment three lines

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use App\Http\Controllers\usercontroller;//importing controller
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');            
-});
+// Route::get('/', function () {
+//     return view('welcome');            
+// });
 
 
 // Route::get('/', function() {
