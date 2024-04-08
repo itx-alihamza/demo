f2ba535c8f6b71a517a5d9ea7c5823bc35db76c8
itx.alihamza <itx.alihamza.pro@gmail.com>
2024-04-08 18:23:21 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,5 @@ use App\Http\Controllers\usercontroller;//importing controller
 */
 
 Route::get('/', function () {
-    return view('welcome');            
+    return view('welcome');
 });
-
-
-// Route::get('/', function() {
-//     return redirect("info");
-// });
-
-// Route::get('/{name}', function($same){
-//     //echo "The name is : ".$name;//. is used for concatenation and value of name will be given by the user at the time of url.
-//     return view('welcome',["name"=>$same]);//here name is the url and welcome is the page which is mapped to that url and page is in the view folder.
-// });
-
-//here
-Route::view("contact", 'contact');
-Route::view("info", 'about');
-
-Route::get('/info', function(){ 
-    return view('about');
-});
-
-Route::get("user/{id}", [usercontroller::class, 'show']);
